5d0143df7d884a30d2928ed9f460110951e959c2
Olal2 <eduherruiz@hotmail.com>
2025-05-08 08:17:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def whatsapp_reply():
 def obtener_respuesta_openai(historial):
     try:
         respuesta = client.chat.completions.create(
-            model="mistralai/mixtral-8x7b-instruct",  # ← Puedes cambiarlo por otro modelo de OpenRouter
+            model="openchat/openchat-3.5", #LLM modelo de OpenRouter
             messages=historial
         )
         return respuesta.choices[0].message.content.strip()
